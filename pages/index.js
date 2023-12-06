aaf88a18e23eda9b1e0b883999f21dd1ad950aba
petitlunette <147147487+petitlunette@users.noreply.github.com>
2023-12-06 11:44:30 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+
+import React from 'react';
+import Blog from "@/components/Blog.js"; // Correct the import path
+import bg1 from 'path/to/bg1'; // Import your images
+import bg2 from 'path/to/bg2';
+import bg3 from 'path/to/bg3';
+import bg4 from 'path/to/bg4';
+import { Row, Col } from 'react-bootstrap'; // Import Row and Col from your UI library
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
@@ -16,8 +24,59 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-
+      <Row>
+        {BlogData.map((blg) => (
+          <Col sm="6" lg="6" xl="3" key={blg.title}>
+            <Blog
+              image={blg.image}
+              title={blg.title}
+              subtitle={blg.subtitle}
+              description={blg.description} // Changed from text to description
+              color={blg.btnbg}
+            />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
       <Footer />
     </div>
   )
 }
+
+
+const BlogData = [
+  {
+    image: bg1,
+    title: "Home Gardening",
+    subtitle: "Subtitle",
+    description:
+      "Description line.",
+    btnbg: "primary",
+  },
+  {
+    image: bg2,
+    title: "Seasonal Produce",
+    subtitle: "Subtitle",
+    description:
+      "Description line.",
+    btnbg: "primary",
+  },
+  {
+    image: bg3,
+    title: "Alternative Protein",
+    subtitle: "Subtitle",
+    description:
+      "Description line.",
+    btnbg: "primary",
+  },
+  {
+    image: bg4,
+    title: "Pesticides and anti-monoculture",
+    subtitle: "Subtitle",
+    description:
+      "Description line.",
+    btnbg: "primary",
+  },
+];
